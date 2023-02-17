a921921ff6a8600756a5b8c7c203619596ecac67
Freitag-fri <sanya007ua@gmail.com>
2023-02-17 20:28:16 +0200

    added atribute and communityId for achievement

--- src/achievement.ts
+++ src/achievement.ts
@@ -1,21 +1,22 @@
 import { Achievement } from "../generated/schema";
 import { Address, BigInt, ByteArray, Bytes, ipfs, json, JSONValueKind } from '@graphprotocol/graph-ts'
 import { getPeeranhaNFT } from "./utils";
+import { getPeeranhaUser } from "./utils";
 import { getUser } from "./user";
 import { ERROR_IPFS, isValidIPFS } from "./utils";
 
 
 
 export function newAchievement(achievement: Achievement | null, achievementId: BigInt): void {
- let peeranhaAchievement = getPeeranhaNFT().getAchievementsNFTConfig(achievementId);
- if (!peeranhaAchievement) return;
+  let peeranhaAchievement = getPeeranhaNFT().getAchievementsNFTConfig(achievementId);
+  if (!peeranhaAchievement) return;
 
- achievement.factCount = peeranhaAchievement.factCount;
- achievement.maxCount = peeranhaAchievement.maxCount;
- achievement.achievementURI = peeranhaAchievement.achievementURI;
- achievement.achievementsType = peeranhaAchievement.achievementsType;
-
- getIpfsAchievementData(achievement)
+  achievement.factCount = peeranhaAchievement.factCount;
+  achievement.maxCount = peeranhaAchievement.maxCount;
+  achievement.achievementURI = peeranhaAchievement.achievementURI;
+  achievement.achievementsType = peeranhaAchievement.achievementsType;
+  achievement.communityId = getPeeranhaUser().try_getAchievementCommunity(achievementId);
+  getIpfsAchievementData(achievement)
 }
 
 export function addDataToAchievement(achievement: Achievement | null, achievementId: BigInt): void {
@@ -36,28 +37,47 @@ function getIpfsAchievementData(achievement: Achievement | null): void {
       if(isValidIPFS(ipfsData)) {
         let ipfsObj = ipfsData.toObject()
         let name = ipfsObj.get('name');
-        if (!name.isNull()) {
+        if (!name.isNull() && name.kind == JSONValueKind.STRING) {
           achievement.name = name.toString();
         }
 
         let description = ipfsObj.get('description');
-        if (!description.isNull()) {
+        if (!description.isNull() && description.kind == JSONValueKind.STRING) {
           achievement.description = description.toString();
         }
 
-        // let attributes = ipfsObj.get('attributes');
-        // if (!attributes.isNull()) {
-        //   achievement.attributes = attributes.toString();
-        // }
+        let attributes = ipfsObj.get('attributes');
+        if (!attributes.isNull() && attributes.kind == JSONValueKind.ARRAY) {
+          const translationsArray = translations.toArray();
+          const translationsLength = translationsArray.length;
+
+          for (let i = 0; i < translationsLength; i++) {
+            const translationsObject = translationsArray[i].toObject();
+            const trait_type = translationsObject.get("trait_type");
+            const value = translationsObject.get("value");
+            if (!trait_type.isNull() && trait_type.kind == JSONValueKind.STRING) {
+              if(trait_type == "Community Id") {
+                if (!value.isNull() && value.kind == JSONValueKind.STRING)
+                  achievement.atrCommunityId = value;
+              } else if (trait_type == "Event") {
+                if (!value.isNull() && value.kind == JSONValueKind.STRING)
+                  achievement.atrEvent = value;
+
+              } else if (trait_type == "Type") {
+                if (!value.isNull() && value.kind == JSONValueKind.STRING)
+                  achievement.atrType = value;
+              }
+            }
+          }
+        }
 
         let image = ipfsObj.get('image');
-        if (!image.isNull()) {
+        if (!image.isNull() && image.kind == JSONValueKind.STRING) {
           achievement.image = image.toString();
         }
       } else {
         achievement.name = ERROR_IPFS;
         achievement.description = ERROR_IPFS;
-        achievement.attributes = ERROR_IPFS;
         achievement.image = ERROR_IPFS;
       }
     }
