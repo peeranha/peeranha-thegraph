4a2cc5d0bea0134423f1072f68f60f9aa9f3735e
Dmitrijg24k <dmitrij3e5g@gmail.com>
2023-03-07 13:05:23 +0300

    feature/BLOC-37 : add documentationCount to community

--- src/post.ts
+++ src/post.ts
@@ -497,6 +497,9 @@ export function generateDocumentationPosts(
       }
   });
   oldPosts = uniqueOldPosts;
+  let community = getCommunity(comunityId);
+  community.documentationCount = posts.length ? posts.length-1 : posts.length;
+  community.save();
   for (let index = 0; index < posts.length; index++) {
     newPosts.push(posts[index]);
   }
@@ -526,6 +529,7 @@ export function generateDocumentationPosts(
       post.author = userAddr.toHex();
       post.communityId = comunityId;
       post.lastmod = lastmodTimestamp;
+      post.isDeleted = false;
       post.postType = PostType.Documentation;
       post.ipfsHash = ByteArray.fromHexString(newPosts[index]) as Bytes;
   
